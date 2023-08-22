97a3e72c2ce1bec3f665f520550033930b788e3d
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-08-22 14:20:28 -0400

    created task list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get all
+  get 'tasks' => 'tasks#index'
+
+  # create
+  get 'tasks/new' => 'tasks#new', as: :new_task
+  post 'tasks' => 'tasks#create'
+
+  # update
+  get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
+  patch 'tasks/:id' => 'tasks#update'
+
+  # delete
+  delete 'tasks/:id' => 'tasks#destroy'
+
+  # read one
+  get 'tasks/:id' => 'tasks#show', as: :task
 end
